0c3443497c419bf0da9e525573efb892d92f64bb
diatsilva007 <diatsilva007@gmail.com>
2025-12-06 15:02:12 -0300

    Adiciona funcionalidade de logout e atualiza o gerenciamento de estado do usuário

--- src/redux/user/slice.js
+++ src/redux/user/slice.js
@@ -9,24 +9,28 @@ export const userSlice = createSlice({
   initialState,
   reducers: {
     createUser: (state, action) => {
-
       return {
         ...state,
         user:{
           name: action.payload.name,
           email: action.payload.email,
-          address: {
-            location: "Rua paraiba",
-            number: 34
-          },
+          address: null,
         }
+      }
+
+    },
+    logoutUser: (state) => {
+
+      return {
+        ...state,
+        user: null,
       }
 
     }
   }
 })
 
-export const { createUser } = userSlice.actions;
+export const { createUser, logoutUser } = userSlice.actions;
 
 export default userSlice.reducer;
 
